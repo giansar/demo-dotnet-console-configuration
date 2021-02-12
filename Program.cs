844478e990edd6e86aa9ff6daf4d84a55942865a
Giansar Pratiknya <gpratiknya@gmail.com>
2021-02-12 21:50:22 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace demo_dotnet_console_configuration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build();
+            Console.WriteLine(configuration["name"]);
+        }
+    }
+}
